32e68fb52d9c313e30b23d455cbae49cc6d8f951
ende151@163.com <ende151@163.com>
2024-03-12 09:09:12 +0800

    提交

---
+++ src/04_父子组件通讯/AppIndex.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AppIndex extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AppIndex;
