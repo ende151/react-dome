8dd65777d07e1b5102cb65ff99f520cfd02f06a5
ende151@163.com <ende151@163.com>
2024-05-29 18:06:33 +0800

    新增redux学习文件

---
+++ src/30_redux/01_Redux的基本理念/AppIndex.jsx
@@ -0,0 +1,17 @@
+import React, {PureComponent} from 'react'
+
+export class App extends PureComponent {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <>
+                我是基础内容 哈哈哈
+            </>
+        )
+    }
+}
+
+export default App
